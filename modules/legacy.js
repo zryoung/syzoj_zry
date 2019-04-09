bf3edbe26322c6d4f2a385fb25831d0f48d76a84
zryoung <riyang@qq.com>
2019-04-09 10:01:41 +0800

    disable register

--- modules/legacy.js
+++ modules/legacy.js
@@ -13,3 +13,15 @@ app.get('/judge_state', async (req, res) => {
 app.get('/judge_detail/:id', async (req, res) => {
   res.redirect('/submission/' + req.params.id);
 });
+
+app.get('/sign_up', async (req, res) => { 
+  if (res.locals.user) { 
+    res.render('error', { 
+      err: new ErrorMessage('您已经登录了，请先注销。', { '注销': syzoj.utils.makeUrl(['logout'], { 'url': req.originalUrl }) })
+    }); 
+  } else { 
+    res.render('error', { 
+      err: new ErrorMessage('暂时不开放注册。', { '登录': syzoj.utils.makeUrl(['login'], { 'url': req.originalUrl }) }) 
+    }); 
+  } 
+});
